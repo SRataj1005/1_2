9b0d10f2f317228883199556275dc90391d9d278
SRataj1005 <100698720+SRataj1005@users.noreply.github.com>
2022-03-09 13:46:04 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+class Main 
+{
+  public static void main(String[] args) 
+  {
+    int a = 1;
+    double b = 2;
+    char c = 's';
+    String d = "SeBaStIaN";
+    
+    
+    System.out.println("a = " + a + "\nb = " + b + "\nc = " + c + "\nd = " +d);
+  }
+}
